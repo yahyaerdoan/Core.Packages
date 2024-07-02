893a11ea6437fc35b28811484aae11990089e42d
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-07-02 14:25:48 -0500

    Base entity developed.

---
+++ Core.PersistenceLayer/Repositories/Entities/Entity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.PersistenceLayer.Repositories.Entities;
+
+public class Entity<TId>
+{
+    public TId Id { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime? UpdatedDate { get; set; }
+    public DateTime? DeletedDate { get; set; }
+    public Entity()
+    {
+        Id = default;
+    }
+    public Entity(TId id)
+    {
+        Id = id;
+    }
+}
