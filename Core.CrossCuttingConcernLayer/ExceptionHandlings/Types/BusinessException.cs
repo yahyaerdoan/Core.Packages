02e2c90a4e71b42f79f99b073e777dbc7625486e
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-07-09 09:55:34 -0500

    Cross cutting concerns created. Business exceptions developed.

---
+++ Core.CrossCuttingConcernLayer/ExceptionHandlings/Types/BusinessException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcernLayer.ExceptionHandlings.Types
+{
+    public class BusinessException : Exception
+    {
+        public BusinessException(){ }
+        public BusinessException(string? message) : base(message) { }
+        public BusinessException(string? message, Exception? innerException) : base(message, innerException) { }      
+    }
+}
