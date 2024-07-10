9cf7b14d4b5dc79b05f1bd979fff9081271f4b8c
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-07-09 20:35:32 -0500

    Transaction implemented.

---
+++ Core.ApplicationLayer/Pipelines/Transactions/Concretions/TransactionScopeBehavior.cs
@@ -0,0 +1,31 @@
+﻿using Core.ApplicationLayer.Pipelines.Transactions.Abstractions;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Core.ApplicationLayer.Pipelines.Transactions.Concretions;
+
+public class TransactionScopeBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>, ITransactionalRequest
+{
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        using TransactionScope transactionScope = new(TransactionScopeAsyncFlowOption.Enabled);
+        TResponse response;
+        try
+        {
+            response = await next();
+            transactionScope.Complete();
+        }
+        catch (Exception)
+        {
+
+            transactionScope.Dispose();
+            throw;
+        }
+        return response;
+    }
+}
