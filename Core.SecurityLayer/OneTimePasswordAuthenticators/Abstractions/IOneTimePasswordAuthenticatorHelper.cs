01b8e21bf046a0ea4e48ea90e2c13ce9243ac7c2
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-08-19 20:41:41 -0500

    One time password net has been implemented.

---
+++ Core.SecurityLayer/OneTimePasswordAuthenticators/Abstractions/IOneTimePasswordAuthenticatorHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.SecurityLayer.OptAuthenticators.Abstractions;
+
+public interface IOneTimePasswordAuthenticatorHelper
+{
+    public Task<byte[]> GenerateSecretKey();
+    public Task<string> ConvertSecretKeyToString(byte[] secretKey);
+    public Task<bool> VerifyCode(byte[] secretKey, string code);
+}
---
+++ Core.SecurityLayer/OneTimePasswordAuthenticators/Concretions/OneTimePasswordNet/OtpNetOneTimePasswordAuthenticatorHelper.cs
@@ -0,0 +1,40 @@
+﻿using Core.SecurityLayer.OptAuthenticators.Abstractions;
+using OtpNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+;
+namespace Core.SecurityLayer.OneTimePasswordAuthenticators.Concretions.OneTimePasswordNet
+{
+    public class OtpNetOneTimePasswordAuthenticatorHelper : IOneTimePasswordAuthenticatorHelper
+    {
+        public Task<byte[]> GenerateSecretKey()
+        {
+            byte[] key = KeyGeneration.GenerateRandomKey(20);
+
+            string base32String = Base32Encoding.ToString(key);
+            byte[] base32Bytes = Base32Encoding.ToBytes(base32String);
+
+            return Task.FromResult(base32Bytes);
+        }
+
+        public Task<string> ConvertSecretKeyToString(byte[] secretKey)
+        {
+            string base32String = Base32Encoding.ToString(secretKey);
+            return Task.FromResult(base32String);
+        }
+
+        public Task<bool> VerifyCode(byte[] secretKey, string code)
+        {
+            Totp totp = new(secretKey);
+
+            string totpCode = totp.ComputeTotp(DateTime.UtcNow);
+
+            bool result = totpCode == code;
+
+            return Task.FromResult(result);
+        }
+    }
+}
