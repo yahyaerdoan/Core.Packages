09b0ec71d160181cbf1c7deda62021420d88bc88
Yahya Erdogan <yahyaerdoan@gmail.com>
2026-08-07 22:19:14 -0500

    Make PageRequest props nullable, update NuGet package version
    Changed PageIndex and PageSize in PageRequest.cs to nullable int (int?).
    Updated NuGet package version in Directory.Build.props from 1.0.5 to 1.0.11-beta.11.

--- Core.ApplicationLayer/Requests/Page/PageRequest.cs
+++ Core.ApplicationLayer/Requests/Page/PageRequest.cs
@@ -2,6 +2,6 @@
 
 public class PageRequest
 {
-    public int PageIndex { get; set; }
-    public int PageSize { get; set; }
+    public int? PageIndex { get; set; }
+    public int? PageSize { get; set; }
 }
